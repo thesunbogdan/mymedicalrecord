61eab042bd745fc9eb932907ec40a9c226af276d
thesunbogdan <dosanbogdan20@gmail.com>
2022-05-24 16:48:07 +0300

    commit before refactoring

--- src/components/list/list.components.jsx
+++ src/components/list/list.components.jsx
@@ -3,6 +3,17 @@ import { DataGrid } from "@mui/x-data-grid";
 import { connect } from "react-redux";
 import { columns } from "../../utils/patients-list-data";
 import { getUsersByRole } from "../../utils/firebase";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 class ListComponent extends React.Component {
   constructor() {
@@ -10,6 +21,50 @@ class ListComponent extends React.Component {
     this.state = {
       patientsAllowedRaw: [],
       selectedPatients: null,
+      data: [
+        {
+          name: "Page A",
+          uv: 4000,
+          pv: 2400,
+          amt: 2400,
+        },
+        {
+          name: "Page B",
+          uv: 3000,
+          pv: 1398,
+          amt: 2210,
+        },
+        {
+          name: "Page C",
+          uv: 2000,
+          pv: 9800,
+          amt: 2290,
+        },
+        {
+          name: "Page D",
+          uv: 2780,
+          pv: 3908,
+          amt: 2000,
+        },
+        {
+          name: "Page E",
+          uv: 1890,
+          pv: 4800,
+          amt: 2181,
+        },
+        {
+          name: "Page F",
+          uv: 2390,
+          pv: 3800,
+          amt: 2500,
+        },
+        {
+          name: "Page G",
+          uv: 3490,
+          pv: 4300,
+          amt: 2100,
+        },
+      ],
     };
   }
 
@@ -64,6 +119,13 @@ class ListComponent extends React.Component {
         ) : (
           ""
         )}
+        <ResponsiveContainer width="50%" height="50%">
+          <BarChart width={150} height={40} data={this.state.selectedPatients}>
+            <XAxis dataKey="lastName" />
+            <YAxis />
+            <Bar dataKey="age" fill="#8884d8" />
+          </BarChart>
+        </ResponsiveContainer>
       </div>
     );
   }
